8849bbbdc3186530b0eba048a89af044a916ee2d
Tim Ryan <tim@3drobotics.com>
2015-06-11 12:13:30 -0700

    Adds setup.py for convenience.

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='Dronekit SITL Runner',
+      version='1.0',
+      description='',
+      author='Tim Ryan',
+      author_email='tim@3drobotics.com',
+      url='https://github.com/tcr3dr/dronekit-sitl-runner/',
+      scripts=['dronekit-sitl'],
+      )

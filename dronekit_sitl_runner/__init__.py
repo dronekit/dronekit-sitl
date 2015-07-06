d8dba071054bb0e9743ffb0c54122018f0a213e2
Tim Ryan <tim@3drobotics.com>
2015-07-06 13:36:40 -0700

    Writes SITL binaries to home dir, not to python dir.

--- dronekit_sitl_runner/__init__.py
+++ dronekit_sitl_runner/__init__.py
@@ -16,9 +16,10 @@ import os
 import json
 import shutil
 from subprocess import Popen
+from os.path import expanduser
 
 sitl_host = 'http://d3jdmgrrydviou.cloudfront.net'
-self = os.path.dirname(os.path.realpath(__file__))
+sitl_target = expanduser('~/.dronekit/sitl')
 
 def version_list():
     sitl_list = '{}/versions.json'.format(sitl_host)
@@ -31,14 +32,16 @@ def version_list():
 def download(system, version, target):
     sitl_file = "{}/{}/sitl-{}-v{}.tar.gz".format(sitl_host, system, target, version)
 
-    if not os.path.isdir(self + '/sitl/' + system + '-' + version):
+    if not os.path.isdir(sitl_target + '/' + system + '-' + version):
         print("Downloading SITL from %s" % sitl_file)
 
-        testfile = urllib.URLopener()
-        testfile.retrieve(sitl_file, self + '/sitl.tar.gz')
+        os.makedirs(sitl_target)
 
-        tar = tarfile.open(self + '/sitl.tar.gz')
-        tar.extractall(path=self + '/sitl/' + system + '-' + version)
+        testfile = urllib.URLopener()
+        testfile.retrieve(sitl_file, sitl_target + '/sitl.tar.gz')
+
+        tar = tarfile.open(sitl_target + '/sitl.tar.gz')
+        tar.extractall(path=sitl_target + '/' + system + '-' + version)
         tar.close()
 
         print('Extracted.')
@@ -46,10 +49,10 @@ def download(system, version, target):
         print("SITL already Downloaded.")
 
 def launch(system, version, args):
-    args = [self + '/sitl/' + system + '-' + version + '/ArduCopter.elf'] + args
+    args = ['./' + system + '-' + version + '/ArduCopter.elf'] + args
     print('Execute:', str(args))
 
-    p = Popen(args, cwd=self)
+    p = Popen(args, cwd=sitl_target)
     p.communicate()
 
 def detect_target():
@@ -61,5 +64,5 @@ def detect_target():
 
 def reset():
     # delete local sitl installations
-    shutil.rmtree(self + '/sitl/')
+    shutil.rmtree(sitl_target + '/')
     print('SITL directory cleared.')

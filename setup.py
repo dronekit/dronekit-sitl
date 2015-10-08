6088a2425765ed51dd7919b4f97002990cf8980a
Tim Ryan <tim@3drobotics.com>
2015-10-08 10:10:41 -0400

    2.2.0

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup, find_packages
 
 setup(name='dronekit_sitl',
-      version='2.1.2',
+      version='2.2.0',
       description='Runs SITL as part of DroneKit.',
       author='Tim Ryan',
       author_email='tim@3drobotics.com',

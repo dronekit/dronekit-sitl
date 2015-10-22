2a44d6bb90f3dd5434105f65db8b7556330463aa
Tim Ryan <tim@3drobotics.com>
2015-10-21 18:43:24 -0700

    Bumps release version.

--- setup.py
+++ setup.py
@@ -3,14 +3,14 @@
 from setuptools import setup, find_packages
 
 setup(name='dronekit_sitl',
-      version='2.4.2',
+      version='3.0.0',
       description='DroneKit library to run SITL (simulation environment)',
       author='Tim Ryan',
       author_email='tim@3drobotics.com',
       url='https://github.com/dronekit/dronekit-sitl/',
       install_requires = [
         'psutil>=3.0',
-        'dronekit>=2.0.0b5',
+        'dronekit>=2.0.0b6',
       ],
       package_data={
         'dronekit_sitl': ['*.parm'],
